9641bbb066f6da5aa9eca8297edf34d7ca8927fc
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-25 23:50:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var s1 = true;
+var pi = 3.14;
+
+var a = 3;
+var b = 4;
+
+console.log(a, b);
+
+var c;
+c = 420;
+console.log(c);
+
+var y = a * b;
+console.log(y.toString());
+console.log(y.toFixed(2));
+
+y = (b * a).toString();
+
+console.log(y);
